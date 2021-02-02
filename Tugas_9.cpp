d54e6379917ef5b1ac3edf88e4cf2fde83fa1cb8
Riki Andriana <rikiandrianakuliah@gmail.com>
2021-02-02 11:16:25 +0700

    Membuat Take Home Pay

---
+++ Tugas_9.cpp
@@ -0,0 +1,35 @@
+//program menghitung gaji bersih karyawan
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+//deklarasi konstanta
+const float persentunjangan = 0.2;
+const float persenpajak = 0.15;
+
+int main(){
+	
+	//deklarasi variable
+	string namakaryawan;
+	float gajibersih, gajipokok, pajak, tunjangan;
+	//input nama karyawan
+	cout<<"Masukan Nama Karyawan : ";
+	getline(cin,namakaryawan);
+	//input gaji pokok
+	cout<<"Masukan Gaji Pokok : Rp . ";
+	cin>>gajipokok;
+	//penghitungan tunjangan
+	tunjangan = persentunjangan * gajipokok;
+	//penghitungan pajak
+	pajak = persenpajak * gajipokok;
+	//penghitungan gaji bersih
+	gajibersih = gajipokok + tunjangan - pajak;
+	//tampilan akhir (nama & jumlah gaji bersih karyawan)
+	cout<<"Gaji Bersih "<<'"'<<namakaryawan<<'"'<<" adalah : ";
+	cout<<fixed;
+	cout<<setprecision(0);
+	cout<<"Rp . "<<gajibersih;
+	
+	return 0;
+}
